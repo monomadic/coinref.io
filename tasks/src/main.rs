893f10a167e433a96b786cfba99c33973f23cead
Rob Saunders <rob@wearebrandnew.com>
2018-01-16 17:55:15 +0700

    muted fetch script

--- tasks/src/main.rs
+++ tasks/src/main.rs
@@ -1,3 +1,30 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate restson;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct CoinmarketCapGlobal {
+    total_market_cap_usd: f64,
+    total_24h_volume_usd: f64,
+}
+
+impl restson::RestPath<()> for CoinmarketCapGlobal {
+    fn get_path(_: ()) -> Result<String, restson::Error> { Ok("/v1/global/".to_string()) }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct CoinmarketCap {
+    id: String,
+    symbol: String,
+}
+
+impl restson::RestPath<()> for CoinmarketCap {
+    fn get_path(_: ()) -> Result<String, restson::Error> { Ok("/v1/ticker/".to_string()) }
+}
+
 fn main() {
-    println!("fetching market cap data...");
+    // println!("fetching market cap data...");
+    // let mut client = restson::RestClient::new("https://api.coinmarketcap.com").unwrap();
+    // let data: CoinmarketCap = client.get(()).unwrap();
+    // println!("{:?}", data);
 }
