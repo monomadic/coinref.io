d791af39f25f741a4511ce5ff03b20a1a8937321
Rob Saunders <rob@wearebrandnew.com>
2018-04-10 16:17:51 +0700

    removed unnecessary shell scripts

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# cargo build --target=x86_64-unknown-linux-musl --release
-cargo build --all
--- nukedb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm database.sql
-rm src/schema.rs
-diesel setup --database-url ./database.sql
-diesel print-schema --database-url ./database.sql > src/schema.rs
